2664437e361c0da7d87e4471c98dccd947181a0a
kxp9472 <karthikp94@gmail.com>
2018-04-18 16:51:10 -0500

    gdt

--- gdt.h
+++ gdt.h
@@ -2,7 +2,54 @@
 #define GDT_H
 
 
+#include"types.h"
 
+class globalDescriptorTable{
+
+	public:
+	class segmentDescriptor{
+	private:
+		uint16_t segmentLimitLow;
+		uint16_t baseAddressLow;
+		uint8_t  baseAddressHigh;
+		uint8_t  type;
+		uint8_t segmentLimitHigh;
+		uint8_t baseAddressMSB;
+	public:
+		segmentDescriptor(uint32_t base,uint32_t limit,uint8_t type);
+		uint32_t baseRearrange();
+		uint32_t limitRearrange();
+
+
+
+
+
+
+	}__atrribute__((packed));
+
+	private:
+		segmentDescriptor codeSegmentSelector;
+		segmentDescriptor dataSegmentSelector;
+		segmentDescriptor nullSegmentSelector;
+		segmentDescriptor unusedSegmentSelector;
+
+	public:
+		globalDescriptorTable();
+		~globaleDescriptorTable();
+
+		uint16_t codeSegmentSelector();
+		uint16_t dataSegmentSelector();
+
+
+
+
+
+
+
+
+
+
+};
 
 
 
