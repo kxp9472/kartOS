b90adb2099539b2e6f9edda5bcc43037f16ee811
kxp9472 <karthikp94@gmail.com>
2018-04-18 19:56:06 -0500

    incomplete gdt

--- gdt.cpp
+++ gdt.cpp
@@ -1,1 +1,20 @@
+
 #include "gdt.h"
+
+globalDescriptorTable::globalDescriptorTable():codeSegmentSelector(0,64*1024*1024,0x9A),
+		      dataSegmentSelector(0,64*1024*1024,0x92),
+		      nullSegmentSelector(0,0,0),
+		      unusedSegmentSelector(0,0,0){
+
+uint64_t loadPtr;
+loadPtr=(sizeof(globalDescriptorTable)-1)<<16;
+loadPtr|=((uint32_t)this)<<32;
+__asm volatile("lgdt (%0)"::"p"(((uint8_t*)loadPtr)+2));
+
+
+}
+
+globalDescriptorTable::~globalDescriptorTable(){
+}
+
+
